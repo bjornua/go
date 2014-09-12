44fe16a6fc806d1cc3793ac5e6260e4312cde423
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2014-09-12 13:57:30 +0200

    Performance

--- anagram.go
+++ anagram.go
@@ -7,7 +7,7 @@ import (
     "bufio"
     "sort"
     "fmt"
-    "bytes"
+//    "bytes"
     "runtime/pprof"
 )
 
@@ -25,7 +25,7 @@ func parseWordlist(r io.Reader) sortedstringlist  {
 }
 
 type sortedstring struct {
-    sorted   string
+    sorted   []rune
     original string
 }
 type sortedstringlist []sortedstring
@@ -33,62 +33,62 @@ func (p sortedstringlist) Len() int           { return len(p) }
 func (p sortedstringlist) Less(i, j int) bool { return p[i].original < p[j].original }
 func (p sortedstringlist) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
+
+type RuneSlice []rune
+func (p RuneSlice) Len() int           { return len(p) }
+func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
+func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+
 func NewSortedString(s string) sortedstring {
-    var letters []string
+    var letters RuneSlice
     for _, x := range s {
-        letters = append(letters, string(x))
+        letters = append(letters, x)
     }
-    sort.Strings(letters)
-    return sortedstring{strings.Join(letters, ""), s}
+    sort.Sort(letters)
+    return sortedstring{letters, s}
 }
 
-func subtractletters(base string, subtrahend sortedstring) (bool, string) {
-    var rest bytes.Buffer
+func subtractletters(base RuneSlice, subtrahend sortedstring) (bool, RuneSlice) {
+    var rest RuneSlice
     i := 0
     j := 0
     for {
         if i >= len(subtrahend.sorted) {
-            rest.WriteString(base[j:])
+            for _, r := range base[j:] {
+                rest = append(rest, r)
+            }
             break
         }
         if j >= len(base) {
-            return false, ""
+            return false, RuneSlice{}
         }
         
-        b := base[j]
-        s := subtrahend.sorted[i]
-
-        if b < s {
-            rest.WriteString(string(b))
-            j++
-        } else if b > s {
-            return false, ""
-        } else {
-            j++
+        if base[j] < subtrahend.sorted[i] {
+            rest = append(rest, base[j])
+        } else if base[j] == subtrahend.sorted[i] {
             i++
+        } else {
+            return false, RuneSlice{}
         }
+        j++
     }
-    return true, rest.String()
+    return true, rest
 }
 
-type anagram struct {
-    word sortedstring
-    pool string
-}
-func anagrammer(depth int, r chan string, pool string, words sortedstringlist) {
+func anagrammer(depth int, r chan string, pool []rune, words sortedstringlist) {
     var validwords []sortedstring
-    var pools []string
+    var pools []RuneSlice
     for _, w := range words {
         success, newpool := subtractletters(pool, w)
-        if !success {
-            continue;
+        if success {
+            pools = append(pools, newpool)
+            validwords = append(validwords, w)
         }
-        validwords = append(validwords, w)
-        pools = append(pools, newpool)
     }
     for i, w := range validwords {
         newpool := pools[i]
-        rr := make(chan string)
+        rr := make(chan string, 2)
         if len(newpool) == 0 {
             r <- w.original
         } else if depth != 2 {
@@ -113,17 +113,24 @@ func main() {
         println("Usage: ./rabbit-hole2 \"anagram\" < path/to/wordlist")
         os.Exit(2)
     }
-
     anagram := os.Args[1]
+
+    var charpool RuneSlice
+    for _, c := range anagram {
+        if c != ' ' {
+            charpool = append(charpool, c)
+        }
+    }
+    sort.Sort(charpool)
 
     words := parseWordlist(os.Stdin)
     sort.Sort(words)
 
-    result := make(chan string)
-    go anagrammer(0, result, strings.Trim(NewSortedString(anagram).sorted, " "), words)
+    result := make(chan string, 100)
+    go anagrammer(0, result, charpool, words)
     
     for x := range result {
-        fmt.Printf("%s = %s\n", anagram, x)
+        fmt.Printf("%s = %s\n", string(anagram), x)
     }
 
     //fmt.Printf("%#v\n", words[:50])

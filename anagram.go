85b002cbd472987dac1c29bc5ebc98e899a9333a
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2014-09-13 22:08:15 +0200

    Make things faster

--- anagram.go
+++ anagram.go
@@ -7,37 +7,36 @@ import (
     "bufio"
     "sort"
     "fmt"
+    "sync"
+    "flag"
 //    "bytes"
     "runtime/pprof"
 )
 
 
-func parseWordlist(r io.Reader) sortedstringlist  {
-    var xs sortedstringlist
+func parseWordlist(r io.Reader) []string {
+    var xs []string
     s := bufio.NewScanner(r)
     for s.Scan() {
-        t := s.Text()
-        t = strings.Trim(t, " \n")
-        t = strings.ToLower(t)
-        xs = append(xs, NewSortedString(t))
+        xs = append(xs, s.Text())
     }
     return xs
 }
-
-type sortedstring struct {
-    sorted   []rune
-    original string
-}
-type sortedstringlist []sortedstring
-func (p sortedstringlist) Len() int           { return len(p) }
-func (p sortedstringlist) Less(i, j int) bool { return p[i].original < p[j].original }
-func (p sortedstringlist) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 
 type RuneSlice []rune
 func (p RuneSlice) Len() int           { return len(p) }
 func (p RuneSlice) Less(i, j int) bool { return p[i] < p[j] }
 func (p RuneSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+type sortedstring struct {
+    sorted   RuneSlice
+    original string
+}
+type sortedstringlist []sortedstring
+/*func (p sortedstringlist) Len() int           { return len(p) }
+func (p sortedstringlist) Less(i, j int) bool { return p[i].original < p[j].original }
+func (p sortedstringlist) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }*/
 
 
 func NewSortedString(s string) sortedstring {
@@ -51,8 +50,12 @@ func NewSortedString(s string) sortedstring {
 
 func subtractletters(base RuneSlice, subtrahend sortedstring) (bool, RuneSlice) {
     var rest RuneSlice
+    //rest := make(RuneSlice, 0, len(base))
     i := 0
     j := 0
+    if  len(subtrahend.sorted) > len(base)  {
+        return false, RuneSlice{}
+    }
     for {
         if i >= len(subtrahend.sorted) {
             for _, r := range base[j:] {
@@ -63,7 +66,6 @@ func subtractletters(base RuneSlice, subtrahend sortedstring) (bool, RuneSlice) 
         if j >= len(base) {
             return false, RuneSlice{}
         }
-        
         if base[j] < subtrahend.sorted[i] {
             rest = append(rest, base[j])
         } else if base[j] == subtrahend.sorted[i] {
@@ -76,8 +78,43 @@ func subtractletters(base RuneSlice, subtrahend sortedstring) (bool, RuneSlice) 
     return true, rest
 }
 
-func anagrammer(depth int, r chan string, pool []rune, words sortedstringlist) {
-    var validwords []sortedstring
+func anagrammer(original string, words []string, maxdepth int) chan []string {
+    var xs []string
+    for _, t := range words {
+        t = strings.Trim(t, " \n")
+        t = strings.ToLower(t)
+        if len(t) > 0 {
+            xs = append(xs, t)
+        }
+    }
+    sort.Strings(xs)
+    var xs_final sortedstringlist
+    for i := range xs {
+        if i == 0 || xs[i] != xs[i-1] {
+            xs_final = append(xs_final, NewSortedString(xs[i]))
+        }
+    }
+    var charpool RuneSlice
+    for _, c := range original {
+        if c != ' ' {
+            charpool = append(charpool, c)
+        }
+    }
+    sort.Sort(charpool)
+    r := make(chan []string)
+    var wg sync.WaitGroup
+
+    wg.Add(1)
+    go anagrammer_r(&wg, maxdepth, []string{}, r, charpool, xs_final)
+    go func() {
+        wg.Wait()
+        close(r)
+    }()
+    return r
+}
+
+func anagrammer_r(wg *sync.WaitGroup, maxdepth int, prefix []string, r chan []string, pool []rune, words sortedstringlist) {
+    var validwords sortedstringlist
     var pools []RuneSlice
     for _, w := range words {
         success, newpool := subtractletters(pool, w)
@@ -88,17 +125,15 @@ func anagrammer(depth int, r chan string, pool []rune, words sortedstringlist) {
     }
     for i, w := range validwords {
         newpool := pools[i]
-        rr := make(chan string, 2)
+        new_prefix := append(prefix, w.original)
         if len(newpool) == 0 {
-            r <- w.original
-        } else if depth != 2 {
-            go anagrammer(depth + 1, rr, newpool, validwords)
-            for a := range rr {
-                r <- w.original + " " + a
-            }
+            r <- new_prefix
+        } else if len(prefix) != maxdepth {
+            wg.Add(1)
+            anagrammer_r(wg, maxdepth, new_prefix, r, newpool, validwords)
         }
     }
-    close(r)
+    wg.Done()
 }
 
 func main() {
@@ -109,28 +144,24 @@ func main() {
     pprof.StartCPUProfile(f)
     defer pprof.StopCPUProfile()
 
-    if len(os.Args) != 2 {
-        println("Usage: ./rabbit-hole2 \"anagram\" < path/to/wordlist")
-        os.Exit(2)
-    }
-    anagram := os.Args[1]
-
-    var charpool RuneSlice
-    for _, c := range anagram {
-        if c != ' ' {
-            charpool = append(charpool, c)
-        }
-    }
-    sort.Sort(charpool)
+    depth := flag.Int("maxwords", -1, "Maximum words")
+    flag.Parse()
+    anagram := flag.Arg(0)
+    println(anagram)
+    println(*depth)
 
     words := parseWordlist(os.Stdin)
-    sort.Sort(words)
-
-    result := make(chan string, 100)
-    go anagrammer(0, result, charpool, words)
+    result := anagrammer(anagram, words, *depth)
     
-    for x := range result {
-        fmt.Printf("%s = %s\n", string(anagram), x)
+    for r := range result {
+        fmt.Print(r[0])
+        for i := 1; i < len(r); i++ {
+            fmt.Print(" ")
+            fmt.Print(r[i])
+       }
+       fmt.Println()
+
+        //fmt.Printf("%s = %s\n", string(anagram), x)
     }
 
     //fmt.Printf("%#v\n", words[:50])
